4b87f70b4c15fecb2d276406e016994f5cc3314f
YangKJ <393103982@qq.com>
2016-12-22 13:57:56 +0800

    12-22
    12-22

---
+++ commonFunction/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  commonFunction
+//
+//  Created by 杨科军 on 2016/12/20.
+//  Copyright © 2016年 杨科军. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
